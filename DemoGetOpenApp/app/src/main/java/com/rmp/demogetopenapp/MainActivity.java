840a4e64286c349948357c00f15cad23ad324f5e
Joseph Jennings <josephjennings@engr2-2-73-222-dhcp.int.colorado.edu>
2016-11-01 14:55:02 -0600

    Polling for applications used in the Foreground

--- DemoGetOpenApp/app/src/main/java/com/rmp/demogetopenapp/MainActivity.java
+++ DemoGetOpenApp/app/src/main/java/com/rmp/demogetopenapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        //startActivity(intent);
+
 
         Switch looper = (Switch) findViewById(R.id.loopLogButton);
         looper.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
