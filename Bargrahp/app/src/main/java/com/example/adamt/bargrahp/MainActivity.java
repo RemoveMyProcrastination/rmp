4d1757d2696c0345e41f5122522c0c3027f20e3c
ATamm <adta1723@colorado.edu>
2016-12-07 22:20:02 -0800

    stuff

--- Bargrahp/app/src/main/java/com/example/adamt/bargrahp/MainActivity.java
+++ Bargrahp/app/src/main/java/com/example/adamt/bargrahp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.adamt.bargrahp;
 
+import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.LimitLine;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -28,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     BarChart barChart;
     BarChart horChart;
     String[] values = new String[20];
+    LimitLine limitLine;
+
+
+
 
 
     @Override
@@ -46,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
         dailyData.add(e1);
         Entry e2 = new Entry("Facebook", 40);
         dailyData.add(e2);
+        Entry e3 = new Entry("Snapchat", 50);
+        dailyData.add(e3);
+        Entry e4 = new Entry("Instagram", 60);
+        dailyData.add(e4);
 
 
         for (int i = 0; i < dailyData.size(); i++) {
@@ -61,18 +72,28 @@ public class MainActivity extends AppCompatActivity {
             XAxis xAxis = horChart.getXAxis();
             xAxis.setGranularity(1f);
             xAxis.setValueFormatter(new MyAxisValueFormatter(values));
+            YAxis yAxis = horChart.getAxisLeft();
+
+            LimitLine limitLine = new LimitLine(20f, "Procrastination Goal");
+            limitLine.setLineColor(Color.RED);
+            limitLine.setLineWidth(4f);
+            limitLine.setTextColor(Color.BLACK);
+            limitLine.setTextSize(12f);
+            yAxis.addLimitLine(limitLine);
+
+
 
 
 
 
             BarDataSet set = new BarDataSet(barEntries, "WeeklyUsage");
-
+            String desc = "usage";
 
             set.setColors(ColorTemplate.VORDIPLOM_COLORS);
 
             BarData data = new BarData(set);
 
-            data.setBarWidth(0.5f); // set custom bar width
+            data.setBarWidth(.5f); // set custom bar width
            /* barChart.setData(data);
             barChart.setDrawGridBackground(true);
             barChart.setFitBars(true);
@@ -83,8 +104,13 @@ public class MainActivity extends AppCompatActivity {
             horChart.setDrawGridBackground(true);
             horChart.setFitBars(true);
             horChart.invalidate();
+            horChart.setVisibleXRangeMaximum(20); // allow 20 values to be displayed at once on the x-axis, not more
+            horChart.setDragEnabled(true);
+            horChart.setTouchEnabled(true);
+            horChart.setDescription(");
 
-            // barChart.invalidate(); // refresh
+
+        // barChart.invalidate(); // refresh
 
 
         }
